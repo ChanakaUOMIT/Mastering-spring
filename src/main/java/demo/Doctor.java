36e5a0a4895320145434ba99045f34c72f44322d
Chanaka <chanakawi@mitesp.com>
2022-05-23 10:48:45 +0530

    bean configurations

--- src/main/java/demo/Doctor.java
+++ src/main/java/demo/Doctor.java
@@ -1,6 +1,22 @@
 package demo;
 
-public class Doctor {
+public class Doctor implements Staff {
+    private  String qualifications;
+
+    public Doctor(String qualifications) {
+        this.qualifications = qualifications;
+    }
+
+    public String getQualifications() {
+        return qualifications;
+    }
+
+    public void setQualifications(String qualifications) {
+        this.qualifications = qualifications;
+    }
+
+
+
     public void assist(){
         System.out.println("Doctor is assisting");
     }
--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -1,9 +1,21 @@
 package demo;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        Doctor doctor = new Doctor();
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+        Doctor doctor = context.getBean(Doctor.class);
         doctor.assist();
 
+        Nurse nurse =(Nurse)  context.getBean("nurse");
+        nurse.assist();
+
+        Doctor staff =context.getBean(Doctor.class);
+        staff.assist();
+        System.out.println(staff.getQualifications());
     }
 }
